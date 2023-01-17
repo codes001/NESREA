4793edef1d0d38f361ee0d9a7980c16f0063c7e2
Effiong Prince <95102901+Effiongprince19@users.noreply.github.com>
2023-01-17 12:16:41 +0100

    Added Slider

--- script.js
+++ script.js
@@ -33,3 +33,34 @@ function checkBoxes() {
         }
     })
 }
+
+var slides = document.querySelectorAll(".slide");
+var btns = document.querySelectorAll(".slider-btn");
+let currentSlide = 1;
+
+var repeat = function(activeClass){
+    let active = document.getElementsByClassName('active');
+    let i = 1;
+
+    var repeater = () => {
+        setTimeout(function(){
+            [...active].forEach((activeSlide) => {
+                activeSlide.classList.remove('active');
+            });
+
+            slides[i].classList.add('active');
+            btns[i].classList.add('active');
+            i++;
+
+            if(slides.length == i) {
+                i = 0;
+            }
+            if(i >= slides.length){
+                return;
+            }
+            repeater();
+        }, 6000);
+    }
+    repeater();
+}
+repeat();
